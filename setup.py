3ea1e25bc10b2eeb5d2bb1a51252c161f51d016e
Jérôme Pin <jeromepin@users.noreply.github.com>
2023-04-08 15:53:51 +0200

    add HCL support and bump to 1.6.0

--- setup.py
+++ setup.py
@@ -4,38 +4,38 @@ import setuptools
 
 from Cython.Build import cythonize
 
-init = (pathlib.Path('tree_sitter_languages') / '__init__.py').read_text()
-match = re.search(r"^__version__ = '(.+)'$", init, re.MULTILINE)
+init = (pathlib.Path("tree_sitter_languages") / "__init__.py").read_text()
+match = re.search(r"^__version__ = \"(.+)\"$", init, re.MULTILINE)
 version = match.group(1)
 
-with open('README.rst') as reader:
+with open("README.rst") as reader:
     readme = reader.read()
 
 setuptools.setup(
-    name='tree_sitter_languages',
+    name="tree_sitter_languages",
     version=version,
-    description='Binary Python wheels for all tree sitter languages.',
+    description="Binary Python wheels for all tree sitter languages.",
     long_description=readme,
-    author='Grant Jenks',
-    author_email='contact@grantjenks.com',
-    url='https://github.com/grantjenks/py-tree-sitter-languages',
-    license='Apache 2.0',
-    ext_modules=cythonize('tree_sitter_languages/core.pyx', language_level='3'),
-    packages=['tree_sitter_languages'],
-    package_data={'tree_sitter_languages': ['languages.so', 'languages.dll']},
-    install_requires=['tree-sitter'],
+    author="Grant Jenks",
+    author_email="contact@grantjenks.com",
+    url="https://github.com/grantjenks/py-tree-sitter-languages",
+    license="Apache 2.0",
+    ext_modules=cythonize("tree_sitter_languages/core.pyx", language_level="3"),
+    packages=["tree_sitter_languages"],
+    package_data={"tree_sitter_languages": ["languages.so", "languages.dll"]},
+    install_requires=["tree-sitter"],
     project_urls={
-        'Documentation': 'https://github.com/grantjenks/py-tree-sitter-languages',
-        'Source': 'https://github.com/grantjenks/py-tree-sitter-languages',
-        'Tracker': 'https://github.com/grantjenks/py-tree-sitter-languages/issues',
+        "Documentation": "https://github.com/grantjenks/py-tree-sitter-languages",
+        "Source": "https://github.com/grantjenks/py-tree-sitter-languages",
+        "Tracker": "https://github.com/grantjenks/py-tree-sitter-languages/issues",
     },
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: Implementation :: CPython',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
     ],
 )
--- tree_sitter_languages/__init__.py
+++ tree_sitter_languages/__init__.py
@@ -3,8 +3,8 @@
 
 from .core import get_language, get_parser
 
-__version__ = '1.5.0'
-__title__ = 'tree_sitter_languages'
-__author__ = 'Grant Jenks'
-__license__ = 'Apache 2.0'
-__copyright__ = '2022, Grant Jenks'
+__version__ = "1.6.0"
+__title__ = "tree_sitter_languages"
+__author__ = "Grant Jenks"
+__license__ = "Apache 2.0"
+__copyright__ = "2022, Grant Jenks"
